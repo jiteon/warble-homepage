d7fda2d3a56a1eaf04888778bf483fba8bd04429
jiteon <88531142+jiteon@users.noreply.github.com>
2023-12-05 20:43:01 +0800

    Add files via upload

---
+++ src/baseComponents/Button/jiteon-button.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="jiteon-button-box">
+    <div class="btn">
+      <a :class="{animation:useAnimation}" href="#">{{title}}</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "jiteon-button",
+  props: {
+    title: {
+      type: String,
+      default: "按钮"
+    },
+    backgroundColor: {
+      type: String,
+      default: "rgba(16,143,111,0.3)"
+    },
+    useAnimation: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup() {}
+};
+</script>
+
+<style lang="scss">
+.jiteon-button-box {
+  font-size: 15px;
+  .btn {
+    position: relative;
+    min-width: 70px;
+    height: 30px;
+  }
+  &:hover .animation {
+    background: v-bind("backgroundColor");
+  }
+  a {
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(255,255,255,0.05);
+    box-shadow: 0 5px 15px rgba(0,0,0,0.2);
+    border-top: 1px solid rgba(255,255,255,0.1);
+    border-radius: 5px;
+    color: var(--color-text);
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
+//引入全局样式
 import './style.css'
+// import 'styles/index.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
